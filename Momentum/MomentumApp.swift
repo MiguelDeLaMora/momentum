87e8fc46a4d77776590e7d7d5b56468a812efdf9
Miguel de la Mora <miguelalejandrodelamoraarocha@gmail.com>
2025-01-01 12:15:30 -0600

    Initial Commit

---
+++ Momentum/MomentumApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentumApp.swift
+//  Momentum
+//
+//  Created by Miguel De La Mora on 01/01/25.
+//
+
+import SwiftUI
+
+@main
+struct MomentumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
